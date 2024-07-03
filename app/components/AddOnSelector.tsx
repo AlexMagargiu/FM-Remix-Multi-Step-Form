61849b3ed975d7968cdf72c3892eaf9c0dcf9dd9
AlexMagargiu <alexandrumaga2000@gmail.com>
2024-07-03 14:59:08 +0300

    Fix bug

--- app/components/AddOnSelector.tsx
+++ app/components/AddOnSelector.tsx
@@ -35,7 +35,8 @@ const AddOnSelector = ({
   };
 
   return (
-    <div
+    <label
+      htmlFor={addOnId}
       className={`flex w-full cursor-pointer items-center gap-6 rounded-lg border border-neutral-lightGray px-4 py-3 text-primary-marineBlue ${
         isChecked ? "border-primary-purplishBlue bg-neutral-magnolia" : ""
       }`}
@@ -65,10 +66,7 @@ const AddOnSelector = ({
         name={isChecked ? "addOnPriceYearly" : ""}
         value={isChecked && billingCycle === "yearly" ? addOnPriceYearly : ""}
       />
-      <label
-        htmlFor={addOnId}
-        className="flex w-full cursor-pointer items-center justify-between gap-4"
-      >
+      <div className="flex w-full items-center justify-between gap-4">
         <div>
           <p className="font-ubuntu-medium text-sm">{addOnText}</p>
           <p className="text-xs text-neutral-coolGray">{addOnDescription}</p>
@@ -76,8 +74,8 @@ const AddOnSelector = ({
         <p className="text-sm text-primary-purplishBlue">
           {billingCycle === "monthly" ? addOnPriceMonthly : addOnPriceYearly}
         </p>
-      </label>
-    </div>
+      </div>
+    </label>
   );
 };
 
